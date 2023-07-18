6f98bf412ede8307c651053f95e8ad7e1804ca32
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-07-18 10:47:02 -0700

    explore Expo to work with iPhone and Web.
    Co-authored-by: Eusebie Siebenberg <eusebiedev@users.noreply.github.com>
    Co-authored-by: Christopher Ross Davila <ChrisRDavila@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titleText}>Open up App.js to start working on your app!</Text>
+      <Text style={styles.pText}>I've opened the podbay doors, now what, HAL?</Text>
+      <TextInput style={styles.inputText} defaultValue="What are you doing, Dave?"/>
+      <Text style={styles.titleText}>What if I did this? Aha! My phone can see this!</Text>
+      <Text>Now that the web is open, will my phone see this? Yes.</Text>
+      <Text>Images don't seem to work yet</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +21,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center', 
   },
+  titleText: {
+    fontWeight: 'bold',
+    color: 'skyblue',
+    fontSize: 14
+  },
+  pText: {
+    fontFamily: 'courier',
+    fontSize: 12,
+  },
+  inputText: {
+    borderColor: 'black',
+    borderWidth: 2,
+    height: 30
+  }
 });
